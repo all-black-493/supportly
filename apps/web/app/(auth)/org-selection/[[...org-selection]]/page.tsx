37c621881fe781484f1ac9b7a80a578b7992820a
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-13 19:47:02 +0300

    feat: tenantizing for organizations

---
+++ apps/web/app/(auth)/org-selection/[[...org-selection]]/page.tsx
@@ -0,0 +1,11 @@
+import { OrgSelectionView } from "@/modules/auth/ui/views/org-select-view";
+
+import React from 'react'
+
+const Page = () => {
+  return (
+    <OrgSelectionView />
+  )
+}
+
+export default Page
--- apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { SignUp } from '@clerk/nextjs'
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
 
 const Page = () => {
     return (
         <>
-            <SignUp />
+            <SignUpView />
         </>
     )
 }
