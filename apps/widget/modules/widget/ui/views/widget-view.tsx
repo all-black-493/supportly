a392896906f0695485b66c8d107d5e47c5ccc8fe
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-15 13:05:26 +0300

    create widget session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string;
@@ -14,23 +15,9 @@ export const WidgetView = ({ organizationId }: Props) => {
     return (
         <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
 
-            <WidgetHeader>
-                <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
-                    <p className="text-3xl">
-                        Hi there! 👋
-                    </p>
-                    <p className="text-lg">
-                        How can we assist you today?
-                    </p>
+            <WidgetAuthScreen />
 
-                </div>
-            </WidgetHeader>
-
-            <div className="flex flex-1">
-                Widget View:{organizationId}
-            </div>
-
-            <WidgetFooter />
+            {/* <WidgetFooter /> */}
         </main>
     )
 }
