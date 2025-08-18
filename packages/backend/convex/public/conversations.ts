3b913c0d377c0ff0225f2558f831712552ae315d
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-18 07:03:19 +0300

    chat with ai agent

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,6 +1,8 @@
-import { threadId } from "worker_threads";
 import { mutation, query } from "../_generated/server";
 import { ConvexError, v } from "convex/values"
+import { supportAgent } from "../system/ai/agents/supportAgent";
+import { saveMessage } from "@convex-dev/agent";
+import { components } from "../_generated/api";
 
 
 export const getOne = query({
@@ -22,7 +24,17 @@ export const getOne = query({
         const conversation =  await ctx.db.get(args.conversationId)
 
         if(!conversation){
-            return null
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Conversation not found",
+            })
+        }
+
+        if(conversation.contactSessionId !== session._id){
+            throw new ConvexError({
+                code: "UNAUTHORIZED",
+                message: "Incorrect Session",
+            })
         }
 
         return {
@@ -48,7 +60,17 @@ export const create = mutation({
             })
         }
 
-        const threadId = "123";
+        const { threadId } = await supportAgent.createThread(ctx,{
+            userId:args.organizationId
+        })
+
+        await saveMessage(ctx, components.agent, {
+            threadId,
+            message:{
+                role: "assistant",
+                content: "Hello, how can I help you today ?"
+            }
+        })
 
         const conversationId = await ctx.db.insert("conversations", {
             contactSessionId: session._id,
