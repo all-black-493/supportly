c2bbbe38369f1bd88225374c74997f571cb8905e
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-18 22:00:44 +0300

    Enable Infinite Scrolling

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -15,7 +15,7 @@ import { useMutation } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
 import { Doc } from "@workspace/backend/_generated/dataModel"
 import { useAtomValue, useSetAtom } from "jotai"
-import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget-atoms"
+import { contactSessionIdAtomFamily, organizationIdAtom, screenAtom } from "../../atoms/widget-atoms"
 
 const formSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -23,6 +23,8 @@ const formSchema = z.object({
 })
 
 export const WidgetAuthScreen = () => {
+
+    const setScreen = useSetAtom(screenAtom)
 
     const organizationId=useAtomValue(organizationIdAtom)
     const setContactSessionId = useSetAtom(
@@ -67,6 +69,7 @@ export const WidgetAuthScreen = () => {
         })
 
         setContactSessionId(contactSessionId)
+        setScreen("selection")
     }
     return (
         <>
