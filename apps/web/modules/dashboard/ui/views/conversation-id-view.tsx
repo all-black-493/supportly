bf73f843e4d59bd0b7c890d36e81cc3df4f5f92a
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-21 02:08:02 +0300

    ai tools

--- apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
+++ apps/web/modules/dashboard/ui/views/conversation-id-view.tsx
@@ -1,10 +1,12 @@
 "use client"
 
+import { useInfiniteScroll } from "@workspace/ui/hooks/use-infinite-scroll"
+import { InfiniteScrollTrigger } from "@workspace/ui/components/infinite-scroll-trigger"
 import { toUIMessages, useThreadMessages } from "@convex-dev/agent/react"
 import { api } from "@workspace/backend/_generated/api"
 import { Id } from "@workspace/backend/_generated/dataModel"
 import { Button } from "@workspace/ui/components/button"
-import { useMutation, useQuery } from "convex/react"
+import { useAction, useMutation, useQuery } from "convex/react"
 import { MoreHorizontalIcon, Wand2Icon } from "lucide-react"
 import {
     AIConversation,
@@ -29,6 +31,10 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar"
+import { ConversationStatusButton } from "../components/conversation-status-button"
+import { useState } from "react"
+import { cn } from "@workspace/ui/lib/utils"
+import { Skeleton } from "@workspace/ui/components/skeleton"
 
 const formSchema = z.object({
     message: z.string().min(1, "Message is required")
@@ -50,6 +56,17 @@ export const ConversationIdView = ({
         { initialNumItems: 10, }
     )
 
+    const {
+        topElementRef,
+        handleLoadMore,
+        canLoadMore,
+        isLoadingMore
+    } = useInfiniteScroll({
+        status: messages.status,
+        loadMore: messages.loadMore,
+        loadSize: 10
+    })
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -57,18 +74,73 @@ export const ConversationIdView = ({
         },
     })
 
+    const [isEnhancing, setIsEnhancing] = useState(false)
+    const enhanceResponse = useAction(api.private.messages.enhanceResponse)
+    const handleEnhanceResponse = async () => {
+        setIsEnhancing(true)
+        const currentValue = form.getValues("message")
+
+        try {
+            const response = await enhanceResponse({
+                prompt: currentValue
+            })
+
+            form.setValue("message", response)
+
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsEnhancing(false)
+        }
+    }
     const createMessage = useMutation(api.private.messages.create)
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try{
+        try {
             await createMessage({
                 conversationId,
                 prompt: values.message
             })
             form.reset()
-        }catch(error){
+        } catch (error) {
             console.error(error)
         }
+    }
+
+    const [isUpdatingStatus, setIsUpdatingStatus] = useState(false)
+    const updateConversationStatus = useMutation(api.private.conversations.updateStatus)
+
+    const handleToggleStatus = async () => {
+        if (!conversation) {
+            return
+        }
+
+        setIsUpdatingStatus(true)
+
+        let newStatus: "unresolved" | "resolved" | "escalated"
+
+        if (conversation.status === "unresolved") {
+            newStatus = "escalated"
+        } else if (conversation.status === "escalated") {
+            newStatus = "resolved"
+        } else {
+            newStatus = "unresolved"
+        }
+
+        try {
+            await updateConversationStatus({
+                conversationId,
+                status: newStatus
+            })
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsUpdatingStatus(false)
+        }
+    }
+
+    if(conversation ===undefined || messages.status === "LoadingFirstPage"){
+        return <ConversationIdViewLoading />
     }
     return (
         <div className="flex h-full flex-col bg-muted">
@@ -79,10 +151,24 @@ export const ConversationIdView = ({
                 >
                     <MoreHorizontalIcon />
                 </Button>
+
+                {!!conversation && (
+
+                    <ConversationStatusButton
+                        disabled={isUpdatingStatus}
+                        onClick={handleToggleStatus}
+                        status={conversation.status} />
+                )}
             </header>
 
             <AIConversation className="max-h-[calc(100vh-180px)]">
                 <AIConversationContent>
+                    <InfiniteScrollTrigger
+                        canLoadMore={canLoadMore}
+                        isLoadingMore={isLoadingMore}
+                        onLoadMore={handleLoadMore}
+                        ref={topElementRef}
+                    />
                     {toUIMessages(messages.results ?? [])?.map((message) => (
                         <AIMessage
                             from={message.role === "user" ? "assistant" : "user"}
@@ -114,7 +200,8 @@ export const ConversationIdView = ({
                                 <AIInputTextarea
                                     disabled={
                                         conversation?.status === "resolved" ||
-                                        form.formState.isSubmitting
+                                        form.formState.isSubmitting ||
+                                        isEnhancing
                                     }
                                     onChange={field.onChange}
                                     onKeyDown={(e) => {
@@ -135,9 +222,13 @@ export const ConversationIdView = ({
 
                         <AIInputToolbar>
                             <AIInputTools>
-                                <AIInputButton>
+                                <AIInputButton
+                                    onClick={handleEnhanceResponse}
+                                    disabled={conversation?.status === "resolved" ||
+                                        isEnhancing ||
+                                        !form.formState.isValid}>
                                     <Wand2Icon />
-                                    Enhance
+                                    {isEnhancing ? "Enhancing ..." : "Enhance"}
                                 </AIInputButton>
                             </AIInputTools>
                             <AIInputSubmit
@@ -145,13 +236,58 @@ export const ConversationIdView = ({
                                 disabled={
                                     conversation?.status === "resolved" ||
                                     !form.formState.isValid ||
-                                    form.formState.isSubmitting
+                                    form.formState.isSubmitting ||
+                                    isEnhancing
                                 }
                                 status="ready"
                                 type="submit" />
                         </AIInputToolbar>
                     </AIInput>
                 </Form>
+            </div>
+        </div>
+    )
+}
+
+export const ConversationIdViewLoading = () => {
+    return (
+        <div className="flex h-full flex-col bg-muted">
+            <header className="flex items-center justify-between border-b bg-background p-2.5">
+                <Button disabled size="sm" variant="ghost">
+                    <MoreHorizontalIcon />
+                </Button>
+            </header>
+            <AIConversation className="max-h-[calc(100vh-180px)]">
+                <AIConversationContent>
+                    {Array.from({ length: 8 }, (_, index) => {
+                        const isUser = index % 2 === 0;
+                        const widths = ["w-48", "w-60", "w-72"]
+                        const width = widths[index % widths.length]
+
+                        return (
+                            <div className={cn(
+                                "group flex w-full items-end justify-end gap-2 py-2 [&>div]:max-w-[80%]",
+                                isUser ? "is-user" : "is-assistant flex-row-reverse"
+                            )}
+                                key={index}>
+                                    <Skeleton className={`h-9 ${width} rounded-lg bg-neutral-200`}/>
+                                    <Skeleton className="size-8 rounded-full bg-neutral-200" />
+                            </div>
+                        )
+                    })}
+                </AIConversationContent>
+            </AIConversation>
+            <div className="p-2">
+                <AIInput>
+                    <AIInputTextarea 
+                    disabled
+                    placeholder="Type your response as the organization's operator"
+                    />
+                    <AIInputToolbar>
+                        <AIInputTools />
+                        <AIInputSubmit disabled status="ready" />
+                    </AIInputToolbar>
+                </AIInput>
             </div>
         </div>
     )
--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -1,16 +1,19 @@
 import { ConvexError, v } from "convex/values";
 import { action, query } from "../_generated/server";
-import { internal } from "../_generated/api";
+import { components, internal } from "../_generated/api";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { paginationOptsValidator } from "convex/server";
+import { resolveConversation } from "../system/ai/tools/resolveConversation";
+import { escalateConversation } from "../system/ai/tools/escalateConversation";
+import { saveMessage } from "@convex-dev/agent";
 
 export const create = action({
-    args:{
+    args: {
         prompt: v.string(),
         threadId: v.string(),
         contactSessionId: v.id("contactSessions")
     },
-    handler: async(ctx, args) =>{
+    handler: async (ctx, args) => {
         const contactSession = await ctx.runQuery(
             internal.system.contactSessions.getOne,
             {
@@ -18,10 +21,10 @@ export const create = action({
             }
         )
 
-        if(!contactSession || contactSession.expiresAt < Date.now()){
+        if (!contactSession || contactSession.expiresAt < Date.now()) {
             throw new ConvexError({
-                code:"UNAUTHORIZED",
-                message:"Invalid Session"
+                code: "UNAUTHORIZED",
+                message: "Invalid Session"
             })
         }
 
@@ -32,49 +35,65 @@ export const create = action({
             }
         )
 
-        if(!conversation){
+        if (!conversation) {
             throw new ConvexError({
                 code: "NOT_FOUND",
                 message: "Conversation not found"
             })
         }
 
-        if(conversation.status === "resolved"){
+        if (conversation.status === "resolved") {
             throw new ConvexError({
                 code: "BAD_REQUEST",
-                message:" Conversation resolved"
+                message: " Conversation resolved"
             })
         }
 
-        await supportAgent.generateText(
-            ctx,
-            { threadId: args.threadId },
-            {
-                prompt:args.prompt,
-            }
-        )
+        const shouldTriggerAgent =
+            conversation.status === "unresolved"
+
+        if (shouldTriggerAgent) {
+            await supportAgent.generateText(
+                ctx,
+                { threadId: args.threadId },
+                {
+                    prompt: args.prompt,
+                    tools: {
+                        resolveConversation,
+                        escalateConversation,
+                    }
+                }
+            )
+
+        } else {
+            await saveMessage(ctx, components.agent, {
+                threadId: args.threadId,
+                prompt: args.prompt,
+            })
+        }
+
     }
 })
 
 export const getMany = query({
-    args:{
-        threadId:v.string(),
+    args: {
+        threadId: v.string(),
         paginationOpts: paginationOptsValidator,
         contactSessionId: v.id("contactSessions"),
     },
-    handler: async (ctx, args) =>{
+    handler: async (ctx, args) => {
         const contactSession = await ctx.db.get(args.contactSessionId)
 
-        if (!contactSession || contactSession.expiresAt < Date.now()){
+        if (!contactSession || contactSession.expiresAt < Date.now()) {
             throw new ConvexError({
                 code: "UNAUTHORIZED",
                 message: "Invalid session"
             })
         }
 
-        const paginated = await supportAgent.listMessages(ctx,{
+        const paginated = await supportAgent.listMessages(ctx, {
             threadId: args.threadId,
-            paginationOpts:args.paginationOpts,
+            paginationOpts: args.paginationOpts,
         })
 
         return paginated
