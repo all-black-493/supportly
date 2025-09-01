b48274a462992519c06bed70fc7172467c68c4a0
Jeremy Nyangi <justinjerome401@gmail.com>
2025-09-01 19:30:26 +0300

    Integrations UI and functionality

---
+++ apps/web/modules/integrations/utils.ts
@@ -0,0 +1,30 @@
+import {
+    JAVASCRIPT_SCRIPT,
+    HTML_SCRIPT,
+    NEXTJS_SCRIPT,
+    REACT_SCRIPT,
+    type IntegrationId
+} from "./constants";
+
+export const createScript = (
+    integrationId: IntegrationId,
+    organizationId: string,
+) => {
+    if (integrationId === "html") {
+        return HTML_SCRIPT.replace(/{{ORGANIZATION_ID}}/g, organizationId)
+    }
+
+    if (integrationId === "react") {
+        return REACT_SCRIPT.replace(/{{ORGANIZATION_ID}}/g, organizationId)
+    }
+
+    if (integrationId === "html") {
+        return NEXTJS_SCRIPT.replace(/{{ORGANIZATION_ID}}/g, organizationId)
+    }
+
+    if (integrationId === "javascript") {
+        return JAVASCRIPT_SCRIPT.replace(/{{ORGANIZATION_ID}}/g, organizationId)
+    }
+
+    return ""
+}
